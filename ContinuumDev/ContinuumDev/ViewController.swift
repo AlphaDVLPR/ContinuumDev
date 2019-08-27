4c81e5a7810908e557195b93d75ee9843157c1e5
AlphaDVLPR <alphadvlpr@AlphaDVLPRs-MacBook-Pro.local>
2019-08-27 14:29:52 -0600

    Finished organizing folders and setting up the skeleton UI. This will be a good starting point

---
+++ ContinuumDev/ContinuumDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContinuumDev
+//
+//  Created by AlphaDVLPR on 8/27/19.
+//  Copyright © 2019 AlphaDVLPR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
